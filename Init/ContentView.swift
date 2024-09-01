1be2e573a106299e31c1aefce2d698bd89d43dd7
risaaugusta <augustariris20@gmail.com>
2024-09-01 11:11:21 +0700

    Initial Commit

---
+++ Init/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Init
+//
+//  Created by Risa Augusta on 01/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Init/InitApp.swift
@@ -0,0 +1,17 @@
+//
+//  InitApp.swift
+//  Init
+//
+//  Created by Risa Augusta on 01/09/24.
+//
+
+import SwiftUI
+
+@main
+struct InitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
